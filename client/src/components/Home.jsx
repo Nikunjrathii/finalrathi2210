e9175aa09c09d3d6ca09d11a02e5df934a438223
Nikunj rathi <nikunj9826@gmail.com>
2023-10-22 03:55:36 +0530

    secondcommiy

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Home.css"
+import Hero from './Hero'
+import Ourcompany from './Ourcompany'
+import Iws from './Iws'
+import Wcu from './Wcu'
+import GetQuote from './GetQuote'
+
+
+
+
+// import About from './About'
+const Home = () => {
+  return (
+    <div>
+     <Hero/>
+      <Ourcompany/>
+      <Iws/>
+      <Wcu/>
+      <GetQuote/>
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ client/src/components/Portfolio.jsx
@@ -0,0 +1,16 @@
+// UnderConstruction.js
+import React from 'react';
+import './Portfolio.css';
+
+function Portfolio() {
+  return (
+    <div className="under-construction">
+      <div className="content">
+        <h1>Portfolio Under Construction</h1>
+        <p>We're working on something amazing. Stay tuned for updates!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Portfolio;
